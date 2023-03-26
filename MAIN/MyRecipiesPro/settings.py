88130969d0269bf1136062a3701c073477b8221d
yrenhke <81391683+kamauhenry@users.noreply.github.com>
2023-03-26 17:30:18 +0300

    Django up and running with templates

--- MAIN/MyRecipiesPro/settings.py
+++ MAIN/MyRecipiesPro/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'MyRecipiesPro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'D:\SF\MAIN\RecipieApp\templates'],
+        'DIRS': [r'D:\SF\MAIN\RecipieApp\templates\app'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,9 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# settings.py
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
--- MAIN/RecipieApp/views.py
+++ MAIN/RecipieApp/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home( request ):
-    return render(request, 'app/index.html')
+    return render(request, 'app/home.html',{})
+
+def Recipie( request ):
+    return render(request, 'app/Recipie.html',{})
+
+def recipies( request ):
+    return render(request, 'app/recipies.html',{})
+
+def base( request ):
+    return render(request, 'app/base.html',{})
