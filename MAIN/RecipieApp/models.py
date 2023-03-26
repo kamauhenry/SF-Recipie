10e5f21077fb7905afa279231766064345c01044
yrenhke <81391683+kamauhenry@users.noreply.github.com>
2023-03-26 18:18:42 +0300

    Requirments folder added config auth and user

--- MAIN/RecipieApp/models.py
+++ MAIN/RecipieApp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.text import slugify 
+from django.contrib.auth import get_user_model
 
+
+
+
+User = get_user_model()
 # Create your models here.
+
+class Author(models.Model):
+    user = models.ForeignKey(user, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length= 40, blank=true)
+    slug = slug = models.SlugField(max_length=400, unique=True,blank=True)
+    bio = models.TextField()
+    
+    def save(self,*args, **kwargs):
+        if not self.slug:
+            self.slug= slugify(self.fullname)
+        super(Post, self).save(*args, **kwargs)
+
+
+class Post(models.Model):
+    title =models.Charfield(max_length=350)
+    slug = models.SlugField(max_length=400, unique=True, blank=True)
+    user = models.ForeignKey()
+
+    def save(self,*args, **kwargs):
+        if not self.slug:
+            self.slug= slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
